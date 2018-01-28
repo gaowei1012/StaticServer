f83e54f0d6ffd3463cf980c0bc4f6c3af8a3c08b
gmw-zjw <gaomingwei2017@163.com>
2018-01-28 17:08:21 +0800

    '文件类型判断'

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 const Koa = require('koa')
+const path = require('path')
+const content = require('./util/content')
+const mimes = require('./util/mimes')
+
 const app = new Koa()
 
-app.use((ctx) => {
-    ctx.body = 'Koa'
+
+
+/* 静态资源目录相对于入口文件 app.js的路径 */
+const staticPath = './static';
+
+/* 解析资源类型 */
+function parseMime(url) {
+    let extName = path.extname(url)
+    extName = extName ? extName.slice(1) : 'unknown'
+    return mimes[extName]
+}
+
+app.use( async (ctx) => {
+    // 静态资源目录本地的绝对路径
+    let fullStaticPath = path.join(__dirname, staticPath)
+
+    // 获取静态资源内容
+    let _content = await content(ctx, fullStaticPath)
+
+    // 解析请求内容的类型
+    let _mime = parseMime(ctx.url)
+
+    // 如果有相对的文件类型， 就配置上下文的类型
+    if (_mime) {
+        ctx.type = _mime
+    }
+
+    // 输出静态资源内容
+    if (_mime && mime.indexOf('image/') >= 0 ) {
+
+        // 如果是图片 ， 则用node原生res， 输出二进制数据
+        ctx.res.writeHead(200)
+        ctx.res.write(_content, 'binary')
+        ctx.res.end()
+    } else {
+        
+        // 其他则输出文本
+        ctx.body = _content 
+
+    }
+
 })
 
 app.listen(3000, () => {
