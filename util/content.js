58ac303a8a25d9ad0a9740031eeab9dfe9434779
gmw-zjw <gaomingwei2017@163.com>
2018-01-28 17:14:09 +0800

    读取内容方法

--- util/content.js
+++ util/content.js
@@ -1,3 +1,18 @@
 /**
  *  读取请求内容
  */
+
+ const path = require('path')
+ const fs = require('fs')
+
+ /* 封装读取目录内容方法 */
+ const dir = require('./dir')
+
+ /* 封装读取文件内容方法 */
+ const file = require('./file')
+
+ async function content(ctx, fullStaticPath) {
+     
+    // 封装请求资源的完整绝对路径
+    let reqPath = path.join(fullStaticPath, ctx.url)
+ }
