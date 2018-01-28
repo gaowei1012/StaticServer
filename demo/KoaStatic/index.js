9b52003215e4050228ac63447093aa3a968d1c09
gmw-zjw <gaomingwei2017@163.com>
2018-01-28 21:00:45 +0800

    添加新(koa-static)

---
+++ demo/KoaStatic/index.js
@@ -0,0 +1,20 @@
+const Koa = require('koa')
+const path = require('path')
+const static = require('koa-static')
+
+const app = new Koa()
+
+// 静态资源文件入口
+const staticPath = '/static';
+
+app.use(static(
+    path.join(__dirname, staticPath)
+))
+
+app.use( async (ctx) => {
+    ctx.body = 'hello koa-static'
+})
+
+app.listen(3000, () => {
+    console.log('静态服务启动成功！')
+})
---
+++ demo/KoaStatic/static/js/index.js
@@ -0,0 +1,9 @@
+/**
+ *  function
+ */
+
+ 
+(function( ){
+    alert('hello koa2 static server')
+    console.log('hello koa2 static server')
+  })()
